ebab5eb6483becb023fac82bbcb9ad0f3a785190
George Wheaton <georgewheaton@gmail.com>
2012-03-06 20:05:52 -0600

    fixed clicking, inside/outside bubbles and enlarged mode now working

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 var context;
+
+
+// global vars to hold data for events and bubbles across category
 var currentBubbles = new Array();
+var eventInfo = new Array();
+
+
+// global vars for interactivity mode and which bubble is enlarged for mouse click event handler
+var interactiveMode = true;
+var enlargedBubble;
+
+// radius for bubbles
+var smallRad = 40;
+var largeRad = 80;
+
+
 (document.getElementById( 'pForm' )).style.display = 'none';
 
-function Bubble(x, y) {
+function Bubble(x, y, event) {
 	this.x = x;      // x coordinate
 	this.y = y;      // y coordinate
 	this.enlarged = false;
+	this.event = event; // event class to store all event info for this bubble
 	
 	this.draw = function(context) {		
 		
@@ -14,14 +30,14 @@ function Bubble(x, y) {
 			if (!this.enlarged) {
 				context.fillStyle = "rgba(0,100,180,0.5)";
 				context.beginPath();			
-				context.arc(this.x, this.y, 40, 0, Math.PI*2, true);
+				context.arc(this.x, this.y, smallRad, 0, Math.PI*2, true);
 				context.closePath();
 				context.fill();
 			}
 			else {
 				context.fillStyle = "rgba(0,100,180,0.5)";
 				context.beginPath();			
-				context.arc(this.x, this.y, 80, 0, Math.PI*2, true);
+				context.arc(this.x, this.y, largeRad, 0, Math.PI*2, true);
 				context.closePath();
 				context.fill();
 			}
@@ -29,32 +45,65 @@ function Bubble(x, y) {
 			context.restore();
 	}
 }
+
+function Event(name, date, time, descrip, food) {
+	this.name = name; //  string
+	this.date = date; // string
+	this.time = time; // string
+	this.descrip = descrip; // string
+	this.food = food; // true or false
+}
+
 	
 function mouseDown(e)
-{	
+{
 	var x; var y;
 	
 	if (e.pageX || e.pageY) {
 		x = e.pageX;
 		y = e.pageY;
-	}
+	}	
 	else {
 		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 	}
-	
+
 	x -= this.offsetLeft;
 	y -= this.offsetTop;
+		
+	if (interactiveMode) {	
+		var bubble = getClosestBubble(x, y);
 	
-	var bubble = getClosestBubble(x, y);
-	
-	bubble.enlarged = true;
-	redraw();	
+		if ( withinBubble(bubble, x, y, interactiveMode) ) {
+			
+			bubble.enlarged = true;
+			redraw();
+		
+			enlargedBubble = bubble; // set our global enlarged ptr to this bubble for now
+		
+			interactiveMode = false;
+		}
+	}
+	else { // we have an enlarged bubble, if we click outside it return it to normal and reenter interactive mode
+		var bubble = enlargedBubble; // grab our global
+		
+		if (withinBubble(bubble, x, y, interactiveMode)) {
+			// do nothing yet
+		}
+		else {
+		
+			bubble.enlarged = false;
+			redraw();
+			
+			interactiveMode = true;
+			
+		}
+	}
 }
 
 function getClosestBubble(x, y)
 {
-	var mindist = 100000; var dist; var retBubble;
+	var mindist = 9000; var dist; var retBubble;
 	
 	for (var i in currentBubbles) {
 		dist = distBetweenPoints(x, y, currentBubbles[i].x, currentBubbles[i].y);
@@ -67,6 +116,25 @@ function getClosestBubble(x, y)
 	
 	return retBubble;
 }		
+
+function withinBubble(bubble, x, y, mode)
+{
+	var rad;
+	
+	if (mode)  { // small bubble
+		rad = smallRad;
+	}
+	else { // larger
+		rad = largeRad;
+	}
+	
+	if ( distBetweenPoints(x, y, bubble.x, bubble.y) <= rad ) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 	
 
 function distBetweenPoints(x1, y1, x2, y2)
